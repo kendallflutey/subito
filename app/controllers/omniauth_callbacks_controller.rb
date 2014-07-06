fc3647bc75c84ad94fe9711102446a369172244f
Kendall Flutey <kendallflutey@me.com>
2014-07-07 11:55:08 +1200

    added sessions to show error messages for unsuccessflu login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,6 +5,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 		if business.persisted?
 			sign_in_and_redirect business, notice: "Great, you're all signed in"
 		else
+			session["devise.business_attributes"] = business.attributes
 			redirect_to new_business_registration_url
 		end
 	end
