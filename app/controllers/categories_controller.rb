f8828c2eb3abb5ac0550af7c6061aca19be1c658
Kendall Flutey <kendallflutey@me.com>
2014-07-07 00:58:28 +1200

    updated categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,12 +7,30 @@ class CategoriesController < ApplicationController
 	def show
 		@category = Category.find(params[:id])
 		@deals = Deal.where(category_id: @category.id)
+	  # render json: @deals
 	end
 
   def getdeals
-    @category = Category.find(params[:id])
-    @deals = Deal.where(category_id: @category.id)
+    # @category = Category.find(params[:id])
+    # @deals = Deal.where(category_id: @category.id)
+    # pp cookies["lat_lng"]
+    position = JSON.parse(cookies["lat_lng"])
+    # pp"############################"
+    # puts position
+    # puts position["latitude"]
+    latitude = position["latitude"]
+    longitude = position["longitude"]
+    # pp "****************************************"
+    # pp latitude
+    # pp longitude
+    # pp "**************************************"
+
+    @deals = Deal.near([latitude, longitude], 1000)
+    # pp @deals
+    # @categorized_deals = @deals.select { |deal| deal.category_id == (params[:id]) }
     render json: @deals
   end
+
+
 
 end
