16ff76dea8271bb97b9dd506a311a0d348d632c1
Kendall Flutey <kendallflutey@me.com>
2014-07-06 20:01:20 +1200

    added factory for buisnesses and wrote validation specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,13 +1,19 @@
 FactoryGirl.define do 
 	factory :deal do 
-		sequence(:title) { |n| "I am title number #{n}"}
-		sequence(:description) { |n| "I am description number #{n}"}
-		sequence(:address) { |n| "I am the address number #{n}"}
+		sequence(:title) { |n| "I am title number #{n}" }
+		sequence(:description) { |n| "I am description number #{n}" }
+		sequence(:address) { |n| "I am the address number #{n}" }
 		start_time DateTime.now
 		finish_time DateTime.now
 	end
 
 	factory :invalid_deal, parent: :deal do 
 		title nil
+	end
+
+	factory :business do 
+		sequence(:name) { |n| "I am name number #{n}" }
+		sequence(:address) { |n| "I am address number #{n}" }
+		email "kendall@flutey.com"
 	end
 end
--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 describe Business do
+
+	it { should have_many(:deals) }
+	it { should validate_presence_of :name }
+	it { should validate_presence_of :address }
+	it { should validate_presence_of :email }
+
+	it { should validate_uniqueness_of :email }
+
 end
