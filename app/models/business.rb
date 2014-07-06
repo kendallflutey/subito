df0a21eb6b3f83aba7e7405f567e7cdf05bb65bb
Kendall Flutey <kendallflutey@me.com>
2014-07-07 11:48:50 +1200

    addded method to business controller for signup/in

--- app/models/business.rb
+++ app/models/business.rb
@@ -15,6 +15,14 @@ class Business < ActiveRecord::Base
   validates :email, uniqueness: {message: "The email is already registered, please login if it's yours"}
   validates :email, format: { with: /.+@.+\..{2,}/, message: "This isn't a valid email address"}
 
+  def self.from_omniauth
+    where(auth.slice(:provider, :uid)).first_or_create do |business|
+      business.provider = auth.provider
+      business.uid = auth.uid
+      business.name = auth.info.name
+      business.address = auth.info.location
+    end
+  end
 
 
 end
