384d55bce24fb02ff79bfb5ce8313108e046ef9a
Kendall Flutey <kendallflutey@me.com>
2014-07-07 11:41:11 +1200

    added all method to ominauth controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,1 +1,8 @@
-omniauth_callbacks_controller.rb
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+	def all
+		business = Business.from_omniauth(request.env["omniauth.auth"])
+	end
+
+	alias_method :twitter, :all
+end
