829d8ed82a1f901404c442992c4b32fe5e44b033
Kendall Flutey <kendallflutey@me.com>
2014-07-06 15:56:04 +1200

    created new rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Subito::Application.config.secret_token = '3282642de9ebe38036ec8ae2d7e74e54c11110182be804d7433ecb672c76b3c8e8de4518fe7d5d41c309b181752e71066f43e468b4adee5b64ed9ccda5b805a3'
