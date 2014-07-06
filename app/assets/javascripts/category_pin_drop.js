f467f3ec15e11b85ed1c00c747a9e9cb82c0137b
Kendall Flutey <kendallflutey@me.com>
2014-07-07 00:45:13 +1200

    updated view and added js functions

--- app/assets/javascripts/category_pin_drop.js
+++ app/assets/javascripts/category_pin_drop.js
@@ -1,7 +1,18 @@
-function getGeoLocation() {
-  navigator.geolocation.getCurrentPosition(setGeoCookie, geoFail);
+var userPosition = {
 
 }
+
+function getGeoLocation() {
+  navigator.geolocation.getCurrentPosition(function(position){
+    setGeoCookie(position);
+    setUserPosition(position);
+    initialize();
+  }, geoFail);
+
+}
+function setUserPosition(position){
+  userPosition = position.coords;
+};
 
 function setGeoCookie(position) {
   var latlong= {latitude: position.coords.latitude, longitude: position.coords.longitude};
@@ -15,6 +26,10 @@ function geoFail(data){console.log("geofail", data)}
 
 
 var map;
+
+$('.container').click(function(){
+  $('#nav_bottom').hide();
+});
 
 
 
@@ -31,25 +46,37 @@ function Deal(id, title, description, start_time, finish_time, deal_image, busin
   this.longitude =  longitude;
 }
 
-function createMarker(pos, title, id) {
+function createMarker(pos, title, id, deal_image, description, finish_time) {
     var marker = new google.maps.Marker({
         position: pos,
         map: map,
         title: title,
-        id: id
+        id: id,
+        deal_image: deal_image,
+        description: description,
+        finish_time: finish_time
     });
     google.maps.event.addListener(marker, 'click', function() {
        // alert("I am marker " + marker.id);
-       $('body').append('<div id="popup">'+marker.title+'<br \\>'+'I am marker'+marker.id+'</div>');
+       $('#nav_bottom').hide();
+       $('#nav_bottom').empty();
+       $('#nav_bottom').append('<div id="popup_deal"><div id="deal_image"></div><h4>'+marker.title+'</h4>'+'I am marker'+marker.id+description+'<br //><br //> finishes in: '+marker.finish_time+'</div>');
+       $('#nav_bottom').show("slowly");
+
+        // $('#nav_bottom').slideUp(200, DealDown);
+
 
     });
     return marker;
 }
 
-// var mapCenter = new google.maps.LatLng(-42.301225,178.773961);
+function DealDown(){
+  $('#popup_deal').remove();
+}
+
 function initialize() {
-  getGeoLocation();
-  var myLatlng = new google.maps.LatLng(-41.301225,174.773961);
+  console.log("initialize user position", userPosition);
+  var myLatlng = new google.maps.LatLng(userPosition.latitude,userPosition.longitude);
   var mapOptions = {
     zoom: 14,
     center: myLatlng,
@@ -60,7 +87,8 @@ function initialize() {
   var to_objects = function(hash) {
     var deal_list = [];
     $.each(hash, function(index, item){
-      var deal = new Deal(item["id"], item["title"], item["description"], item["description"], item["finish_time"], item["deal_image"], item["business_id"], item["category_id"], item["latitude"], item["longitude"]);
+      var deal = new Deal(item["id"], item["title"], item["description"], item["description"], item["finish_time"], item["deal_image"], item["business_id"], item["category_id"], item["latitude"], item["longitude"], item["finish_time"]);
+      console.log(item["finish_time"])
       deal_list.push(deal);
     });
     console.log(deal_list);
@@ -72,21 +100,22 @@ function initialize() {
 
     for (var i = 0; i < deal_list.length; i++) {
       var deal = deal_list[i];
-      console.log(deal);
+
 
       var myLatlng = new
        google.maps.LatLng(deal["latitude"], deal["longitude"]);
 
-      // var marker = new google.maps.Marker({
-      //     position: myLatlng,
-      //     map: map,
-      //     title: deal['title'],
-      //     id: i
-      // });
+       var finish = moment(deal['finish_time']).fromNow();
 
-      createMarker(myLatlng, deal['title'], i);
+      marker = createMarker(myLatlng, deal['title'], deal['id'], deal['deal_image'], deal['description'], finish);
+      console.log(marker.finish_time)
     }
   };
+ $('#nav_bottom').click(function(){
+  $('#nav_bottom').hide();
+})
+
+
 
   $.ajax({
     url:'/getdeals',
@@ -98,32 +127,7 @@ function initialize() {
 
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-//   var ekim = {"title": "cheap burgers",
-//             "latitude": -41.29237,
-//             "longitude": 174.77642
-// };
 
-// var jjs = {'title': "$5 steak",
-//             'latitude': -41.29341,
-//             'longitude': 174.78118
-// };
-
-// var fix = {'title': "$2 pies",
-//             'latitude': -41.29759,
-//             'longitude': 174.77327
-// };
-
-// var mollies = {'title': "guiness",
-//             'latitude': -41.301225,
-//             'longitude': 174.773961,
-// };
-
-// var deals = [ekim, jjs, fix, mollies];
-
-
-  // google.maps.event.addListener(marker, 'click', function() {
-  //   console.log('clicked');
-  // });
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', getGeoLocation);
