930f49875fea10a0a5a4e6b1caaeb0b748a151b4
Kendall Flutey <kendallflutey@me.com>
2014-07-08 01:10:15 +1200

    fixed js file, now has no cookies and posts to controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,4 @@
+
 class CategoriesController < ApplicationController
 
 	def index
@@ -9,13 +10,17 @@ class CategoriesController < ApplicationController
 		@deals = Deal.where(category_id: @category.id)
 	end
 
-  def getdeals
-    # @position = JSON.parse(cookies["lat_lng"])
-    latitude = position["latitude"]
-    longitude = position["longitude"]
+  def user_coords
+    latitude = params[:latitude]
+    longitude = params[:longitude]
 
-    @deals = Deal.near([latitude, longitude], 1000)
-    render json: @deals
+    getdeals(latitude, longitude)
+    
+  end
+
+  def getdeals(latitude, longitude)
+    deals = Deal.near([latitude, longitude], 1000)
+    render json: deals
   end
 
   def full_show
