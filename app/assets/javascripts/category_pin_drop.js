4c3b279c8cabb9d68f62d4bdb4c7977aa34b743c
Tim Robinson <timrobinson88@gmail.com>
2014-07-08 11:46:56 +1200

    refactored for loop to .each

--- app/assets/javascripts/category_pin_drop.js
+++ app/assets/javascripts/category_pin_drop.js
@@ -1,52 +1,48 @@
+function haveGeolocation() {
+
+  navigator.geolocation.getCurrentPosition(function(position){
+
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var myLatlng = new google.maps.LatLng(latitude, longitude);
+
+    $.ajax({
+        url: '/categories/user_coords',
+        type: 'POST',
+        data: { latitude: latitude, longitude: longitude, id: categoryId },
+        success: function(data){
+          displayMap();
+          processDeals(data);
+        }
+      });
+
+    function displayMap() {
+
+      var mapOptions = {
+        zoom: 14,
+        center: myLatlng,
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    }
+
+    function processDeals(data) {
+      $.each(data,function(index, deal){
+
+        var markerCoords = new google.maps.LatLng(deal.latitude, deal.longitude);
+        var finishTime = moment(deal.finish_time).fromNow();
+        createMarker(markerCoords, deal.title, deal.id, deal.deal_image, deal.description, finishTime);
+      });
+    }
+  });
+}
+
 function getCoords() {
-   
+
   if (navigator.geolocation) {
-        
-    navigator.geolocation.getCurrentPosition(function(position){
-        
-      var latitude = position.coords.latitude;
-      
-      var longitude = position.coords.longitude;
-      
-      var myLatlng = new google.maps.LatLng(latitude, longitude);
-     
-      $.ajax({
-          url: '/categories/user_coords',
-          type: 'POST',
-          data: { latitude: latitude, longitude: longitude, id: categoryId },
-          success: function(data){
-            displayMap();
-            processDeals(data);
-          }
-        });
-
-      function displayMap() {
-        
-        var mapOptions = {
-          zoom: 14,
-          center: myLatlng,
-          disableDefaultUI: true,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-                  
-        map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-      }
-
-      function processDeals(data) {
-        for (var i = 0; i < data.length; i++) {
-
-          var markerLatitude = data[i]["latitude"];
-          var markerLongitude = data[i]["longitude"];
-          var markerCoords = new google.maps.LatLng(markerLatitude, markerLongitude);
-
-          var finish = moment(data[i]["finish_time"]).fromNow();
-
-          createMarker(markerCoords, data[i]["title"], data[i]["id"], data[i]["deal_image"], data[i]["description"], finish);
-        };
-      }
-
-    });
-
+    haveGeolocation();
   } else {
     alert("Sorry, but your browser doesn't play nice with Subito...");
   }
@@ -71,8 +67,6 @@ function createMarker(markerCoords, title, id, deal_image, description, finish_t
      $('#nav_bottom').append('<div id="popup_deal"><div id="deal_image"></div><h4>'+dealMarker.title+'!</h4>'+'<p id="popup-description">'+dealMarker.description+"</p><p id='pop-up-timer'>"+dealMarker.finish_time+'</p></div>');
      $('#nav_bottom').show("slowly");
   });
-
-  return dealMarker;
 }
 
  
