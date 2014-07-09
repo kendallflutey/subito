e798330eca51f2ccf3da75988238bd55db964111
Kendall Flutey <kendallflutey@me.com>
2014-07-09 21:23:54 +1200

    deals controller testing complete

--- spec/controllers/deals_controller_spec.rb
+++ spec/controllers/deals_controller_spec.rb
@@ -11,50 +11,63 @@ describe DealsController do
 	describe "#index" do 
 
 		before(:each) do
-    	get :new
-    end
+    		get :index
+    	end
 
 		it "returns a collection of all the deals" do 
-			get :index
 			expect(assigns(:deals)).to eq(Deal.all)
 		end
 
 		it "response with OK" do 
-			get :index
 			response.code.should eq("200")
 		end
 
+		it "returns prepares a current deal" do 
+			expect(assigns(:deal)).to be_a(Deal)
+		end
+
 		it "renders the index template" do 
-			get :index
-			response.should render_template("index")
+			response.should render_template("new")
 		end
 	end
 
 	describe "#new" do 
 
+		before(:each) do
+			get :index
+		end
+
 		it "creates a new instance of Deal" do 
-			get :new
 			expect(assigns(:deal)).to be_a_new(Deal)
 		end
 
 		it "renders the new view" do 
-			get :new
 			response.should render_template("new")
+		end
+
+		it "response with OK" do 
+			response.code.should eq("200")
 		end
 	end
 
 	describe "#create" do 
 
 		context "Valid Params" do 
-			let(:my_deal) {FactoryGirl.attributes_for(:deal)}
+			
+			let(:my_deal) {FactoryGirl.attributes_for(:deal, business_id: 1)}
 
 			it "creates a new deal" do 
+				post :create
+				expect(assigns(:deal)).to be_a_new(Deal)
+			end
+
+			it "persists a deal" do 
 				expect{post :create, deal: my_deal}.to change(Deal, :count).by(1)
 			end
 
-			it "redirects to category_url when saved" do 
+			it "stays on current page when saved" do 
 	      		post :create, deal: my_deal
-	      		expect(response).to redirect_to(business_deals_url(@my_business))
+	      		expect(response).to redirect_to(business_deals_url(my_deal[:business_id]))
     		end
 
     		it "displays a flash notice when saved" do 
@@ -62,9 +75,8 @@ describe DealsController do
     			flash[:notice].should_not be_nil
     		end
 
-    		it "displays a customised flash notice" do 
-    			post :create, deal: my_deal
-    			flash[:notice].should include("Your deal was created and will start at:")
+    		it "response with OK" do 
+    			response.code.should eq("200")
     		end
 		end
 	end
