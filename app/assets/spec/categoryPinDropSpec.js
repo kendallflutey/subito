fc2e76f75008728adcb80e06b18c08bf8664138e
Tim Robinson <timrobinson88@gmail.com>
2014-07-09 14:02:39 +1200

    started jasmine tests

--- app/assets/spec/categoryPinDropSpec.js
+++ app/assets/spec/categoryPinDropSpec.js
@@ -1,3 +1,27 @@
 describe("Category Pin Drop", function(){
 
+  describe("ajax call", function() {
+    it("passed parameters to ajax", function(){
+      spyOn($, "ajax");
+      haveGeolocation();
+
+      expect($.ajax).toHaveBeenCalledWith({
+        url: '/categories/user_coords',
+        type: 'POST',
+        data: { latitude: latitude, longitude: longitude, id: categoryId },
+        success: function(data){
+          displayMap();
+          processDeals(data);
+        }
+      });
+    });
+  });
+
+  describe("displayMap", function() {
+    xit("should return a mapOption var zoom property of 14", function() {
+      expect(mapOptions.zoom).toEqual(14);
+    });
+  });
+
+
 });
